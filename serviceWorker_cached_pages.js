0453c8ffc026a5bed00432c572e90df0495b48bc
RoraxHan(kyl) <hkg328@outlook.com>
2019-03-26 05:49:54 -0400

    response with cached file if it's offline

--- serviceWorker_cached_pages.js
+++ serviceWorker_cached_pages.js
@@ -1,10 +1,50 @@
-// Call install event
+// Cache entire website
+const cacheName = 'v1';
+const cacheAssets = [
+  'index.html',
+  'about.html',
+  '/css/style.css',
+  '/js/main.js'
+]
 
-self.addEventListener('install', (e) => {
+
+// Call install event
+self.addEventListener('install', (event) => {
   console.log('servcie worker is installed')
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      console.log('service worker caching all')
+      return cache.addAll(cacheAssets)
+    })
+    .then(() => {
+      self.skipWaiting()
+    })
+  )
 })
 
 // Call activate event
-self.addEventListener('activate', (e) => {
+self.addEventListener('activate', (event) => {
   console.log('activated')
+  // remove unwanted caches
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== cacheName) {
+            console.log('delete old cache')
+            return caches.delete(cache)
+          }
+        })
+      )
+    })
+  )
+})
+
+
+// Call fetch event 
+self.addEventListener('fetch', event => {
+  console.log('service worker: fetching')
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  )
 })
